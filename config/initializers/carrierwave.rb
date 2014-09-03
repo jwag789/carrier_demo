dfa0305db813d40eee3812390adf43574d2ace99
Justin Wagner <jwag789@gmail.com>
2014-09-03 11:04:24 -0400

    Edit configure file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,11 @@
 CarrierWave.configure do |config|
 	config.fog_credentials = {
-		:provider			     => 'AWS',
-		:aws_access_key_id       => ENV['S3_KEY'],
-		:aws_secret_access_key   => ENV['S3_SECRET']
+
+	:provider			     => 'AWS',
+	:aws_access_key_id       => ENV['S3_KEY'],
+	:aws_secret_access_key   => ENV['S3_SECRET']
+
 	}
 
-	if Rails.env.test? || Rails.env.development?
-		config.storage = :file
-		config.enable_processing = false
-		config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
-	else
-		config.storage = :fog
-	end
-	# config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
 	config.fog_directory = "carrierdemo"
-
 end
