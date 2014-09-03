9438527c5c9312fc978cf7f76d590981b9dfe10a
Justin Wagner <jwag789@gmail.com>
2014-09-02 22:48:05 -0400

    Move around sass files for compass

--- app/assets/compass/config.rb
+++ app/assets/compass/config.rb
@@ -1,7 +1,10 @@
+# Require any additional compass plugins here.
+
+#Folder settings
 relative_assets = true      #because we're not working from the root
-css_dir = "stylesheets"          #where the CSS will saved
-sass_dir = "stylesheets"           #where our .scss files are
-images_dir = "images"    #the folder with your images
+css_dir = "../css"          #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+images_dir = "../images"    #the folder with your images
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded # After dev :compressed
