93dcd2f47a1594283fdb2c1b083103ea9e761b48
Justin Wagner <jwag789@gmail.com>
2014-09-02 22:07:46 -0400

    Test for deployment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,19 +4,8 @@ CarrierWave.configure do |config|
 	:provider			     => 'AWS',
 	:aws_access_key_id       => ENV['S3_KEY'],
 	:aws_secret_access_key   => ENV['S3_SECRET'],
-	:region                  => ENV['S3_REGION']
 
 	}
 
-	if Rails.env.test? || Rails.env.cucumber?
-		config.storage = :file
-		config.enable_processing = false
-		config.root = "#{Rails.root}/tmp"
-	else
-		config.storage = :fog
-	end
-
-	config.cache_dir = "#{Rails.root}/tmp/uploads"
-
-	config.s3_access_policy = :public_read
+	config.fog_directory = "carrierdemo"
 end
