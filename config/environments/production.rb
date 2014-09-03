1d8c624e31f8b04751c9de2fd73aa6970943e095
Justin Wagner <jwag789@gmail.com>
2014-09-02 23:03:05 -0400

    Css edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+  config.assets.precompile += ['application.css']
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
